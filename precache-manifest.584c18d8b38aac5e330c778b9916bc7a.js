4813388e2df93ea4c8268e8f0dadbf9a2f66f607
unknown <c.o.n.s.t.a.n.t.tnatsnoc@gmail.com>
2019-10-22 15:18:26 +0300

    Updates

---
+++ precache-manifest.584c18d8b38aac5e330c778b9916bc7a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7a1d1ddacea2a2928be8ebea6af9772f",
+    "url": "/ReactEcommerce/index.html"
+  },
+  {
+    "revision": "3cceee4d367808c2f5bd",
+    "url": "/ReactEcommerce/static/css/main.31c7c205.chunk.css"
+  },
+  {
+    "revision": "f46115aa34930c125208",
+    "url": "/ReactEcommerce/static/js/2.0709bf0f.chunk.js"
+  },
+  {
+    "revision": "3cceee4d367808c2f5bd",
+    "url": "/ReactEcommerce/static/js/main.6de6e467.chunk.js"
+  },
+  {
+    "revision": "e43c1a457a8f9b369862",
+    "url": "/ReactEcommerce/static/js/runtime~main.67b489fd.js"
+  },
+  {
+    "revision": "19ef7b17bc9414c63c7781251ce0d24d",
+    "url": "/ReactEcommerce/static/media/crown.19ef7b17.svg"
+  },
+  {
+    "revision": "30f0707118fb960665491424c115ac44",
+    "url": "/ReactEcommerce/static/media/shopping-bag.30f07071.svg"
+  }
+]);
